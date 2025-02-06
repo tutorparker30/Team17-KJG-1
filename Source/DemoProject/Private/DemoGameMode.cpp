39fae789832bfed5c432616affd841bd96684cef
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-06 11:45:14 +0900

    파일 삭제.

--- Source/DemoProject/Private/DemoGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "DemoGameMode.h"
-#include "DemoCharacter.h"
-#include "DemoPlayerController.h"
-
-ADemoGameMode::ADemoGameMode()
-{
-	DefaultPawnClass = ADemoCharacter::StaticClass();
-	PlayerControllerClass = ADemoPlayerController::StaticClass();
-}

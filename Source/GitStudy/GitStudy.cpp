5014f4d6ce0b26241c011d06f54ca00b9cae2c77
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-07 18:35:13 +0900

    새 프로젝트 생성.
    언리얼 프로젝트의 생성.
    소스트리를 활용한 커밋과 푸시 방법 학습.
    커밋 메세지의 제목과 내용은 한 줄 띄기를 통해 구분한다는 것을 알게 됨.

---
+++ Source/GitStudy/GitStudy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitStudy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitStudy, "GitStudy" );

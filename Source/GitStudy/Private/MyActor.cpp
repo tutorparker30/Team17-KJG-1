65c75d6b6012381701dfc6e3229ce20d14891cd7
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-07 20:17:57 +0900

    언리얼 엔진 실행을 위한 BeginPlay(), Tick(함수) 추가.

--- Source/GitStudy/Private/MyActor.cpp
+++ Source/GitStudy/Private/MyActor.cpp
@@ -5,4 +5,12 @@ AMyActor::AMyActor()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
 
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}

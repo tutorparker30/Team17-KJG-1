393af0f633ed8effb497ddc1599782a5ee471474
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-08 15:43:20 +0900

    파일 삭제.

--- Source/GitStudy/GitStudy.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GitStudy.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitStudy, "GitStudy" );

0eb9aeafc5cb610b21fcf0b10d0149cc62927b07
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-10 09:56:45 +0900

    레포지토리 재활용을 위한 파일 삭제.

--- Source/CharacterClass/Private/KGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "KGameMode.h"
-#include "KCharacter.h"
-#include "KPlayerController.h"
-
-AKGameMode::AKGameMode()
-{
-	DefaultPawnClass = AKCharacter::StaticClass(); //게임 실행 시 캐릭터가 월드에 자동 스폰 됨.(기본 폰클래스 설정.)
-	PlayerControllerClass = AKPlayerController::StaticClass(); //게임 모드에 플레이어 컨트롤러 설정.
-}
